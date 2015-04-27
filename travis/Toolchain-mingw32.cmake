aa63be370f0c073deae26ebb28e96574dee23408
suiheilibe <ntjapanize@yahoo.co.jp>
2015-04-27 22:40:16 +0900

    travis CI setting

--- travis/Toolchain-mingw32.cmake
+++ travis/Toolchain-mingw32.cmake
@@ -5,6 +5,7 @@ SET(CMAKE_SYSTEM_NAME Windows)
 SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+SET(CMAKE_DLLTOOL i686-w64-mingw32-dlltool)
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /opt/mingw32)
