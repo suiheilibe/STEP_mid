637237ecdd4387e4b8fab0f1fc0a7b76044facb7
suiheilibe <ntjapanize@yahoo.co.jp>
2017-05-13 15:16:07 +0900

    Add build scripts

---
+++ script/do_build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+rootdir="$1"
+generator="$2"
+target="$3"
+jobs="$4"
+cmakeopts="$5"
+
+builddir="${rootdir}/build/${target}"
+distdir="${rootdir}/dist/${target}"
+
+if echo "${target}" | grep "^STEP_K" >/dev/null
+then
+    cmakeopts="${cmakeopts} -DSTEP_K=1"
+fi
+
+mkdir -p "${builddir}"
+cd "${builddir}"
+rm -rf *
+cmake ../.. -G "${generator}" ${cmakeopts}
+make -j${jobs}
+mkdir -p "${distdir}"
+cp src/STEP_mid.ste "${distdir}"
+cd "${distdir}"
+strip STEP_mid.ste
