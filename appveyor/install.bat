d69808804167b3cb98a6040ef18eb15ffc45d6ea
suiheilibe <ntjapanize@yahoo.co.jp>
2017-05-18 22:20:05 +0900

    AppVeyor: Remove an unnecessary installation

--- appveyor/install.bat
+++ appveyor/install.bat
@@ -5,5 +5,4 @@ SET "PATH=C:\msys64\usr\bin;%PATH%"
 @echo on
 
 bash -lc "pacman -Syu --noconfirm"
-bash -lc "pacman -S --needed --noconfirm base-devel mingw-w64-i686-toolchain mingw-w64-x86_64-toolchain"
 bash -lc "pacman -S --needed --noconfirm mingw-w64-i686-cmake mingw-w64-x86_64-cmake"
