945fca1bbd5a9b975c0422ff5862b4a62a4fd16a
suiheilibe <ntjapanize@yahoo.co.jp>
2017-05-20 18:00:06 +0900

    AppVeyor: Clear older pacman cache

--- appveyor/install.bat
+++ appveyor/install.bat
@@ -6,3 +6,4 @@ SET "PATH=C:\msys64\usr\bin;%PATH%"
 
 bash -lc "pacman -Syu --noconfirm"
 bash -lc "pacman -S --needed --noconfirm mingw-w64-i686-cmake mingw-w64-x86_64-cmake"
+bash -lc "pacman -Sc --noconfirm"
