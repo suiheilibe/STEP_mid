4f7ed207b384ce2a306ec4ff52814eab78d213c1
suiheilibe <ntjapanize@yahoo.co.jp>
2019-06-29 17:07:03 +0900

    Build matrix test

---
+++ cmake/Toolchain-mingw64.cmake
@@ -0,0 +1,18 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+SET(CMAKE_DLLTOOL x86_64-w64-mingw32-dlltool)
+
+# here is the target environment located
+# SET(CMAKE_FIND_ROOT_PATH  /opt/mingw64)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
