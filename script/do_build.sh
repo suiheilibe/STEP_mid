9cb4a49cb33630cd92e7678bb4eca8e418f73a68
suiheilibe <ntjapanize@yahoo.co.jp>
2019-06-29 16:31:49 +0900

    Fix CMake option settings

--- script/do_build.sh
+++ script/do_build.sh
@@ -9,21 +9,34 @@ buildtype="$4"
 cmakeopts="$5"
 cmakebuildopts="$6"
 
-realcmakeopts=""
+realcmakeopts="${cmakeopts}"
+realcmakebuildopts="${cmakebuildopts}"
+
+cmakebuildconfig=""
 
 if [[ -n "${generator}" ]]
 then
-    realcmakeopts="${cmakeopts} -G \"${generator}\""
-fi
-
-if [[ -n "${buildtype}" ]]
-then
-    realcmakeopts="${cmakeopts} -DCMAKE_BUILD_TYPE=${buildtype}"
+    realcmakeopts="${realcmakeopts} -G \"${generator}\""
 fi
 
 if echo "${target}" | grep "^STEP_K" >/dev/null
 then
     realcmakeopts="${realcmakeopts} -DSTEP_K=1"
+fi
+
+if echo "${generator}" | grep "^Visual Studio" >/dev/null
+then
+    if [[ -n "${buildtype}" ]]
+    then
+        cmakebuildconfig="--config ${buildtype}"
+    else
+        cmakebuildconfig="--config Release"
+    fi
+else
+    if [[ -n "${buildtype}" ]]
+    then
+        realcmakeopts="${realcmakeopts} -DCMAKE_BUILD_TYPE=${buildtype}"
+    fi
 fi
 
 builddir="${rootdir}/build/${target}"
@@ -32,7 +45,7 @@ distdir="${rootdir}/dist/${target}"
 mkdir -p "${builddir}"
 cd "${builddir}"
 eval "cmake ../.. ${realcmakeopts}"
-eval "cmake --build . -- ${cmakebuildopts}"
+eval "cmake --build . ${cmakebuildconfig} -- ${realcmakebuildopts}"
 mkdir -p "${distdir}"
 cp src/STEP_mid.ste "${distdir}"
 cd "${distdir}"
