ced2fb3f0cb237fc11eef5ea0ba02afbc8a39e8d
suiheilibe <ntjapanize@yahoo.co.jp>
2021-11-23 21:03:27 +0900

    Add STEPMidUtil::readMetaEvent test

--- test/test.cpp
+++ test/test.cpp
@@ -9,29 +9,32 @@
 #include "STEPlugin.h"
 
 #include "SMFUtil.h"
+#include "STEPMidUtil.h"
+#include "debug.h"
 
-extern void readMetaEvent(FILE_INFO *pFileMP3, FILE *fp, SMFUtil::MetaEvent *events, int type);
+extern "C" void(WINAPI *STEPSetValue)(FILE_INFO *, FIELDTYPE, LPCTSTR);
 
-TEST_GROUP(SMFUtilTestGroup)
-{
-    void setup()
-    {
+void my_STEPSetValue(FILE_INFO *fi, FIELDTYPE ft, LPCTSTR str) {
+    DEBUGOUT("FILE_INFO = %p, FIELDTYPE = %p, value = %p\n", fi, ft, str);
+};
+
+TEST_GROUP(STEPMidTestGroup) {
+    void setup() {
+        STEPSetValue = my_STEPSetValue;
         errno_t err = _tfopen_s(&fp, _T("test_metaevents.mid"), _T("rb"));
         events = (SMFUtil::MetaEvent *)malloc(sizeof(SMFUtil::MetaEvent) * SMFUtil::META_MAX);
     }
 
-    void teardown()
-    {
+    void teardown() {
         free(events);
         fclose(fp);
     }
-    
-    FILE *fp;
-    SMFUtil::MetaEvent *events;
+
+    FILE *fp = nullptr;
+    SMFUtil::MetaEvent *events = nullptr;
 };
 
-TEST(SMFUtilTestGroup, findMetaEvents)
-{
+TEST(STEPMidTestGroup, TestMain) {
     CHECK_EQUAL(0, SMFUtil::findMetaEvents(fp, events));
     // Text Event
     CHECK_EQUAL(542, events[SMFUtil::MetaEventType::META_COMMENT].length);
@@ -39,6 +42,7 @@ TEST(SMFUtilTestGroup, findMetaEvents)
     CHECK_EQUAL(1553, events[SMFUtil::MetaEventType::META_COPYRIGHT].length);
     // Sequence/Track Name
     CHECK_EQUAL(16361, events[SMFUtil::MetaEventType::META_SEQNAME].length);
+    CHECK_EQUAL(0, STEPMidUtil::readMetaEvent(nullptr, fp, events));
 }
 
 int main(int argc, char *argv[])
