90a36f6c094255b0ab83f64f99b63fa3ec344d86
suiheilibe <ntjapanize@yahoo.co.jp>
2021-11-23 17:18:19 +0900

    Omit gen_version.sh call

--- build_msys.sh
+++ build_msys.sh
@@ -8,6 +8,4 @@ generator="MSYS Makefiles"
 cmakeopts=""
 cmakebuildopts="-j4"
 
-${scriptdir}/script/gen_version.sh > "${scriptdir}/src/version.h"
-
 exec "${script}" "${scriptdir}" "${generator}" "${cmakeopts}" "${cmakebuildopts}" $@
